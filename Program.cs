df88e2a92db509528c228be78f6ff5ac66c51f3a
Przemek <przemekworld@gmail.com>
2023-05-13 23:37:23 +0200

    Ref

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
-﻿using adventofcode2022.Day_1;
-using System.ComponentModel.DataAnnotations;
+﻿
+using adventofcode2022.Day_1;
 
-namespace adventofcode2022
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, adventofcode2022");
+Console.WriteLine("Hello, adventofcode2022");
 
-            Console.WriteLine($"Day 1 - { new Day1().CalculateStar()}");
-            Console.WriteLine($"Day 1 - { new Day1().CalculateSecondStar()}");
-        }
-    }
-}
+Console.WriteLine($"Day 1 - {new Day1().CalculateStar()}");
+Console.WriteLine($"Day 1 - {new Day1().CalculateSecondStar()}");
