fae0108e9b1408cba49080fd422243b78d0e56bd
Przemek <przemekworld@gmail.com>
2023-05-13 23:36:33 +0200

    Day1 works!

---
+++ Day-1/Day1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2022.Day_1
+{
+    internal class Day1
+    {
+        public int CalculateStar()
+        {
+            SortedSet<int> allCalories = GetCalories();
+
+            return allCalories.Last();
+
+        }
+
+        private static SortedSet<int> GetCalories()
+        {
+            var lines = File.ReadAllLines("Day-1/puzzleInput.txt");
+
+            var allCalories = new SortedSet<int>();
+
+
+            var currentElfCalories = 0;
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+
+                    allCalories.Add(currentElfCalories);
+                    currentElfCalories = 0;
+                    continue;
+                }
+
+                currentElfCalories += int.Parse(line);
+            }
+
+            return allCalories;
+        }
+
+        public int CalculateSecondStar()
+        {
+            var allCalories = GetCalories().ToArray();
+
+            return allCalories[^1] + allCalories[^2] + allCalories[^3];
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace adventofcode2022
+﻿using adventofcode2022.Day_1;
+using System.ComponentModel.DataAnnotations;
+
+namespace adventofcode2022
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, adventofcode2022");
+
+            Console.WriteLine($"Day 1 - { new Day1().CalculateStar()}");
+            Console.WriteLine($"Day 1 - { new Day1().CalculateSecondStar()}");
         }
     }
 }
