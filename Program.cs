0edc1d5eba98792e0e538a147b6b7746f5fba204
Przemek <przemekworld@gmail.com>
2023-05-13 23:13:05 +0200

    Refresh

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace adventofcode2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, adventofcode2022");
+        }
+    }
+}
