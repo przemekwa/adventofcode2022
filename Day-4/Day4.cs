b2e7f8a7ff82d14a31e834e243e744ff33bb6ae8
Przemek <przemekworld@gmail.com>
2023-05-17 22:16:05 +0200

    Day4 one star

---
+++ Day-4/Day4.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2022.Day_4
+{
+    internal class Day4 : ICalculateStars
+    {
+        public int CalculateSecondStar()
+        {
+            var lines = File.ReadAllLines("Day-4/puzzleInput.txt");
+
+            foreach (var item in lines
+                .Select(s => s.Split(','))
+                .Select(s => (s[0].Split('-'), s[1].Split('-')))
+                .Select((s) => new
+                {
+                    FirstPair = (int.Parse(s.Item1[0]), int.Parse(s.Item1[1])),
+                    SecondPair= (int.Parse(s.Item2[0]), int.Parse(s.Item2[1])),
+
+                }))
+                    
+                    
+            {
+                (var low1, var high1) = item.FirstPair;
+                (var low2, var high2) = item.SecondPair;
+
+
+            }
+
+
+
+            return 0;
+        }
+
+        public int CalculateStar()
+        {
+            var lines = File.ReadAllLines("Day-4/puzzleInput.txt");
+
+            var result = 0;
+            foreach (var item in lines
+                .Select(s => s.Split(','))
+                .Select(s => (s[0].Split('-'), s[1].Split('-')))
+                .Select((s) => new
+                {
+                    FirstPair = (int.Parse(s.Item1[0]), int.Parse(s.Item1[1])),
+                    SecondPair = (int.Parse(s.Item2[0]), int.Parse(s.Item2[1])),
+
+                }))
+
+
+            {
+                (var firstStart, var firstEnd) = item.FirstPair;
+                (var secondStart, var secondEnd) = item.SecondPair;
+
+                if (firstStart <= secondStart && firstEnd >= secondEnd)
+                {
+                    result++;
+                    continue;
+                }
+
+                if (secondStart <= firstStart && secondEnd >= firstEnd)
+                {
+                    result++;
+                    continue;
+                }
+
+
+
+            }
+
+
+
+            return result ;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using adventofcode2022.Day_1;
 using adventofcode2022.Day_2;
 using adventofcode2022.Day_3;
+using adventofcode2022.Day_4;
 using System.Diagnostics;
 
 Console.WriteLine("Hello, adventofcode2022");
@@ -21,8 +22,13 @@ var startWatch = new Stopwatch();
 //Console.WriteLine(TimeSpan.FromMicroseconds(startWatch.ElapsedMilliseconds));
 
 
+//startWatch.Start();
+//Console.WriteLine($"Day 3 ** - {new Day3().CalculateSecondStar()}");
+//startWatch.Stop();
+//Console.WriteLine(TimeSpan.FromMicroseconds(startWatch.ElapsedMilliseconds));
+
+
 startWatch.Start();
-Console.WriteLine($"Day 3 ** - {new Day3().CalculateSecondStar()}");
+Console.WriteLine($"Day 4 * - {new Day4().CalculateStar()}");
 startWatch.Stop();
 Console.WriteLine(TimeSpan.FromMicroseconds(startWatch.ElapsedMilliseconds));
-
