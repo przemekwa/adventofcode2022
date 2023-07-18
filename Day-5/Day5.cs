9cfc9f898c906f49e0b7e026a92af5521b514aa4
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2023-07-18 15:27:47 +0200

    crane moves parsed

--- Day-5/Day5.cs
+++ Day-5/Day5.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace adventofcode2022.Day_5
 {
     internal class Day5 : ICalculateStars
     {
+
+        private char[,] ship = new char[3, 3]
+        {
+            { '\0','D','\0' },
+            { 'N', 'C', '\0'},
+            { 'Z','M','P'}
+        };
+
         public int CalculateSecondStar()
         {
+            var lines = File.ReadAllLines("Day-5/puzzleInput.txt");
+
+            var craneMoves = new List<List<int>>();
+
+            foreach (var line in lines)
+            {
+                var enumerable = Regex.Matches(line, "[0-9]+").Select((s) => int.Parse(s.Value));
+
+                craneMoves.Add(enumerable.ToList());
+
+
+            }
+
+
             return 0;
         }
 
