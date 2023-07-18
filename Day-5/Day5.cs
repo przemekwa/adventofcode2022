4333e838d5944998ff332be227fa867e62642916
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2023-07-18 14:47:50 +0200

    Ref

---
+++ Day-5/Day5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2022.Day_5
+{
+    internal class Day5 : ICalculateStars
+    {
+        public int CalculateSecondStar()
+        {
+            return 0;
+        }
+
+        public int CalculateStar()
+        {
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,40 +1,33 @@
 ﻿
+using adventofcode2022;
 using adventofcode2022.Day_1;
 using adventofcode2022.Day_2;
 using adventofcode2022.Day_3;
 using adventofcode2022.Day_4;
+using adventofcode2022.Day_5;
 using System.Diagnostics;
 
 Console.WriteLine("Hello, adventofcode2022");
 
-//Console.WriteLine($"Day 1 *- {new Day1().CalculateStar()}");
-//Console.WriteLine($"Day 1 ** - {new Day1().CalculateSecondStar()}");
-
-//Console.WriteLine($"Day 2 * - {new Day2().CalculateStar()}");
-//Console.WriteLine($"Day 2 ** - {new Day2().CalculateSecondStar()}");
-
 
 var startWatch = new Stopwatch();
 
-//startWatch.Start();
-//Console.WriteLine($"Day 3 * - {new Day3().CalculateStar()}");
-//startWatch.Stop();
-//Console.WriteLine(TimeSpan.FromMicroseconds(startWatch.ElapsedMilliseconds));
+
+ICalculateStars day = new Day1();
+
+day = new Day2();
+day = new Day3();
+day = new Day4();
+day = new Day5();
 
 
-//startWatch.Start();
-//Console.WriteLine($"Day 3 ** - {new Day3().CalculateSecondStar()}");
-//startWatch.Stop();
-//Console.WriteLine(TimeSpan.FromMicroseconds(startWatch.ElapsedMilliseconds));
-
-
-//startWatch.Start();
-//Console.WriteLine($"Day 4 * - {new Day4().CalculateStar()}");
-//startWatch.Stop();
-//Console.WriteLine(TimeSpan.FromMicroseconds(startWatch.ElapsedMilliseconds));
 
 startWatch.Start();
-Console.WriteLine($"Day 4 ** - {new Day4().CalculateSecondStar()}");
+Console.WriteLine($"{day} ** - {day.CalculateStar()}");
 startWatch.Stop();
 Console.WriteLine(TimeSpan.FromMicroseconds(startWatch.ElapsedMilliseconds));
 
+startWatch.Start();
+Console.WriteLine($"{day} ** - {day.CalculateSecondStar()}");
+startWatch.Stop();
+Console.WriteLine(TimeSpan.FromMicroseconds(startWatch.ElapsedMilliseconds));
