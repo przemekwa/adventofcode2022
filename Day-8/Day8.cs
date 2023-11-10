9e0ee4f995e9884d7fda3423c1eafae49ec33bfa
Przemek <przemekworld@gmail.com>
2023-11-10 20:18:40 +0100

    Day8 Top Down

--- Day-8/Day8.cs
+++ Day-8/Day8.cs
@@ -8,12 +8,13 @@ namespace adventofcode2022
 {
     internal class Day8 : ICalculateStars
     {
+        private int[,] array;
+
         public string CalculateSecondStar()
         {
             var lines = File.ReadAllLines("Day-8/puzzleInput.txt");
 
             int[,] array = new int[lines.First().Length, lines.Length];
-
 
             for (int i = 0; i < lines.Length; i++)
             {
@@ -23,7 +24,68 @@ namespace adventofcode2022
                 }
             }
 
+            CheckArray(array);
+
+
             return "";
+        }
+
+        private void CheckArray(int[,] array)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    //if (CheckTop(array[i, j], i, j, array))
+                    //{
+                    //    Console.WriteLine($"Top OK: {array[i,j]} ");
+                    //}
+
+                    if (CheckDown(array[i, j], i, j, array))
+                    {
+                        Console.WriteLine($"DOWN OK: {array[i, j]} ");
+                    }
+
+
+
+                }
+            }
+        }
+
+        private bool CheckTop(int toCheckPoint, int i, int j, int[,] array)
+        {
+            var top = i - 1;
+            
+            if (top < 0)
+            {
+                return true;
+            }
+
+            if (array[top,j] < toCheckPoint)
+            {
+                return CheckTop(toCheckPoint, top, j, array);
+            }
+
+            return false;
+
+        }
+
+        private bool CheckDown(int toCheckPoint, int i, int j, int[,] array)
+        {
+            var down = i + 1;
+
+            if (down > 4)
+            {
+                return true;
+            }
+
+            if (array[down, j] < toCheckPoint)
+            {
+                return CheckDown(toCheckPoint, down, j, array);
+            }
+
+            return false;
+
         }
 
         public string CalculateStar()
